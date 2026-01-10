86a69754008b7fbc24efeb785d764e05d37fa784
kmzra <aziraashri07@gmail.com>
2026-01-10 13:54:08 +0000

    what I changed

--- routes/web.php
+++ routes/web.php
@@ -36,8 +36,8 @@ Route::middleware(['auth'])->group(function () {
 
 
     // EDIT PROFILE PAGE
-    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::get('/profile/edit', [ProfileEditController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [ProfileEditController::class, 'update'])->name('profile.update');
 
 
 
